85c9e7a13ae7c41bb5b0afb60911afedb7010abc
G22 <pinheirog972@gmail.com>
2025-07-27 17:56:47 -0300

    usando if. switch e do while

---
+++ Materiais-de-estudos/src/MenuInterativo.java
@@ -0,0 +1,34 @@
+///Mostre um menu com 3 opções e use if ou switch///
+///para executar uma ação baseada na escolha do usuário.///
+
+import java.util.Scanner;
+
+public class MenuInterativo {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int opcao;
+
+        do{
+            System.out.println("======== MENU INTERATIVO ========");
+            System.out.println("1. Iniciar Estrutura de Repetição");
+            System.out.println("2. Sair");
+            System.out.print("Escolha uma opção: ");
+
+            opcao = scanner.nextInt();
+
+            switch (opcao) {
+                case 1:
+                    System.out.println("contando ate 10");
+                    for (int i = 1; i < 11; i++){
+                        System.out.println("Contando: " + i);
+                    }return;
+                case 2:
+                    System.out.println("Saindo do menu interativo.");
+                    break;
+                default:
+                    System.out.println("Opção inválida. Tente novamente.");
+            }
+        } while(opcao != 2);
+      scanner.close();   
+    }  
+}
